76df7ef5e938d93a530c70d486f325f7a0bdd805
slcastroj <sl.castroj@gmail.com>
2020-09-30 21:39:57 -0300

    Readme y configuración externalizada

---
+++ README.md
@@ -0,0 +1,11 @@
+# Readme
+
+Prerrequisitos:
+- Instalar dependencias para cliente y servidor con `npm install`
+- Instalar PostgreSQL y ejecutar scripts de carpeta `sql_scripts`
+- Configurar variables de entorno para conexión a DB y testing
+
+Tanto para ejecutar el servidor como el cliente se debe utilizar `npm start`
+Opcionalmente se puede ejecutar Jest sobre el servidor con `npm test`
+
+El archivo .env para servidor se dejó a propósito en el repositorio, a modo de demostración y no contiene información sensible. En un entorno real, debe haber un archivo de configuración de ejemplo y cada usuario debe completar con los datos que correspondan a su configuración local.
--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,14 @@
 const pgPromise = require('pg-promise');
 
+const pgPort = process.env.POSTGRE_PORT || 5432;
+const pgHost = process.env.POSTGRE_HOST || 'localhost';
+const pgDatabase = process.env.POSTGRE_DATABASE;
+const pgDatabaseTesting = process.env.POSTGRE_DATABASE_TESTING;
+const pgUser = process.env.POSTGRE_USER || 'postgres';
+const pgPass = process.env.POSTGRE_PASS;
+
 const environment = process.env.NODE_ENV || 'prod';
-const database = environment === 'prod' ? 'tcit' : 'tcit-test';
-const connectionString = `postgres://postgres:slcastroj@localhost:5432/${database}`;
+const database = environment === 'prod' ? pgDatabase : pgDatabaseTesting;
+const connectionString = `postgres://${pgUser}:${pgPass}@${pgHost}:${pgPort}/${database}`;
 const db = pgPromise()(connectionString);
 exports.db = db;
